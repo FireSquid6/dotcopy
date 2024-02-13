5bb4bd046e6185159055f654f9c67a2470be1788
firesquid6 <jdeiss06@gmail.com>
2024-02-13 14:08:15 -0600

    add fsnotify

--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,8 @@
 module github.com/firesquid6/dotcopy
 
 go 1.21.5
+
+require (
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+)
---
+++ src/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/fsnotify/fsnotify"
+	"log"
+)
+
+func main() {
+	log.Println("Starting watcher")
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				log.Println("event:", event)
+				if event.Has(fsnotify.Write) {
+					log.Println("modified file:", event.Name)
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add("/tmp")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	<-make(chan struct{})
+}
