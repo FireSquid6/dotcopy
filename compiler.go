a5b6e4919c7b513f0188e9ee1b096ea514b289f5
firesquid6 <jdeiss06@gmail.com>
2024-02-22 12:19:08 -0600

    add compiler

--- compiler.go
+++ compiler.go
@@ -1,9 +1,55 @@
 package main
 
+import (
+	"strings"
+)
+
 func CompileDotfile(dotfile Dotfile) (string, string) {
-	// compiledText := dotfile.TemplateText
+	compiledText := dotfile.TemplateText
 	// iterate through the lines of the SlotText using a FSM and generate compiledText
 	// return compiledText, dotfile.CompiledFilepath
+	slots := getSlots(dotfile.SlotText)
 
-	return "", ""
+	for _, line := range strings.Split(dotfile.TemplateText, "\n") {
+		for _, slot := range slots {
+			if strings.Contains(line, slot.slotText) {
+				compiledText = strings.ReplaceAll(compiledText, slot.slotText, slot.insert)
+			}
+		}
+	}
+
+	return compiledText, dotfile.CompiledFilepath
+}
+
+type Slot struct {
+	insert   string // the text to insert into the template
+	slotText string // the slot text to look for
+}
+
+func getSlots(slotfile string) []Slot {
+	slots := []Slot{}
+	state := "inside" // whether we are inside or outside of a slot
+	currentSlot := Slot{}
+
+	lines := strings.Split(slotfile, "\n")
+
+	for _, line := range lines {
+		if state == "inside" {
+			if strings.HasPrefix(line, "---") {
+				slots = append(slots, currentSlot)
+				state = "outside"
+			} else {
+				currentSlot.insert += line + "\n"
+			}
+
+		} else {
+			if strings.HasPrefix(line, "---") {
+				currentSlot.slotText = strings.Split(line, " ")[1]
+				state = "inside"
+			}
+		}
+
+	}
+
+	return slots
 }
