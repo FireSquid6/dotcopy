9a3c15fc882c78c0eb77868077fe86af9bb9f29f
firesquid6 <jdeiss06@gmail.com>
2024-02-24 11:43:46 -0600

    reduce scope

--- go.mod
+++ go.mod
@@ -2,8 +2,9 @@ module github.com/firesquid6/dotcopy
 
 go 1.21.5
 
+require gopkg.in/yaml.v2 v2.4.0
+
 require (
-	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/radovskyb/watcher v1.0.7
 	golang.org/x/sys v0.4.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0
 )
--- main.go
+++ main.go
@@ -1,45 +1,7 @@
 package main
 
-import (
-	"github.com/fsnotify/fsnotify"
-	"log"
-)
-
 func main() {
-	// I'm like 90% sure this doesn't work right
-	log.Println("Starting watcher")
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer watcher.Close()
-
-	go func() {
-		for {
-			select {
-			case event, ok := <-watcher.Events:
-				if !ok {
-					return
-				}
-				log.Println("event:", event)
-				if event.Has(fsnotify.Write) {
-					log.Println("modified file:", event.Name)
-				}
-			case err, ok := <-watcher.Errors:
-				if !ok {
-					return
-				}
-				log.Println("error:", err)
-			}
-		}
-	}()
-
-	err = watcher.Add("/tmp")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	<-make(chan struct{})
+	Compile()
 }
 
 func Compile() {
